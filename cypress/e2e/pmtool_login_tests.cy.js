1f9a36ebe04e21b4efa5df15095ae2ef9960b37c
Eliška Šuhaj <eliska.suhaj@gopay.cz>
2025-04-07 19:36:42 +0200

    tags

--- cypress/e2e/pmtool_login_tests.cy.js
+++ cypress/e2e/pmtool_login_tests.cy.js
@@ -1,4 +1,4 @@
-describe("PMtool Login Tests", () => {
+describe("PMtool Login Tests", { tags: "@smoke" }, () => {
   it("Title exist", () => {
     cy.visit("https://tredgate.com/pmtool");
     cy.get("h3.form-title").should("exist");
